e4293c57684f45586beadae92d0632eb98cf8a22
Victoria Nogueira Marques <victoriamarques0604@gmail.com>
2024-03-28 18:57:21 -0300

    fix: desktop home

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,7 @@ import { FooterD } from "./home-sections-desktop/footer_section";
 export default function App() {
   return (
     <div className="w-screen h-screen flex flex-col">
-      <div className="min-[700px]:hidden">
+      <div className="mobile min-[700px]:hidden">
         <div className="fixed hamburger-menu w-screen h-24 flex py-9 px-6 bg-primary-white z-50">
           <div>
             <img src={DarkLogo} alt="Designo Logo" className="w-52 h-7" />
@@ -53,15 +53,33 @@ export default function App() {
         </div>
       </div>
 
-      <div className="max-[700px]:hidden px-40">
-        <div className="flex justify-between items-center py-16">
-          <img src={DarkLogo} alt="" className="w-[202px]" />
-          <nav className="flex gap-11">
-            <a href="">OUR COMPANY</a>
-            <a href="">LOCATIONS</a>
-            <a href="">CONTACT</a>
-          </nav>
+      <div className="desktop max-[700px]:hidden px-40">
+        <div className="header flex justify-between bg-gray-900 overflow-hidden my-16">
+          <div className="">
+            <img className="w-[195.02px] h-[20.54px" src={DarkLogo} />
+          </div>
+          <div className="inline-flex justify-center items-start gap-[42px]">
+            <a
+              href="#home"
+              className="ttext-zinc-800 text-sm font-normal font-['Jost'] leading-[14px] tracking-widest"
+            >
+              OUR COMPANY
+            </a>
+            <a
+              href="#news"
+              className="text-zinc-800 text-sm font-normal font-['Jost'] leading-[14px] tracking-widest"
+            >
+              LOCATIONS
+            </a>
+            <a
+              href="#contact"
+              className="text-zinc-800 text-sm font-normal font-['Jost'] leading-[14px] tracking-widest"
+            >
+              CONTACT
+            </a>
+          </div>
         </div>
+
         <FirstSectionD />
         <SecondSectionD />
         <ThirdSectionD />
--- src/home-sections-desktop/first_section.tsx
+++ src/home-sections-desktop/first_section.tsx
@@ -1,9 +1,24 @@
 export function FirstSectionD() {
   return (
-    <div className="">
-      <h1 className="font-medium text-3xl text-primary-black text-center">
-        Hello World
-      </h1>
+    <div className="flex justify-between bg-[url('./src/components/home/desktop/bg-pattern-hero-home.svg')] bg-no-repeat bg-right bg-primary-peach px-24 rounded-[15px] overflow-hidden">
+      <div className="py-36">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-primary-white text-5xl font-medium font-['Jost'] leading-[48px]">
+            Award-winning custom <br></br> designs and digital <br></br>branding
+            solutions
+          </h1>
+          <p className="text-primary-white text-base font-normal font-['Jost'] leading-relaxed">
+            With over 10 years in the industry, we are experienced in <br></br>{" "}
+            creating fully responsive websites, app design, and engaging{" "}
+            <br></br>brand experiences. Find out more about our services.
+          </p>
+        </div>
+        <button className="w-[152px] h-14 bg-primary-white rounded-lg text-secondary-dark_grey text-[15px] font-medium font-['Jost'] uppercase tracking-wide mt-10">
+          Learn More
+        </button>
+      </div>
+
+      <div className="w-[500px] h-[800px] overflow-hidden bg-[url('./src/components/home/desktop/image-hero-phone.png')] bg-no-repeat bg-center relative -bottom-[100px] "></div>
     </div>
   );
 }
